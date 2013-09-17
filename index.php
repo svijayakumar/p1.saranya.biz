9ffc9abbcb9279dbb3e75c43337da5edb31219ca
svijayakumar <saranya@new-host-2.home>
2013-09-16 23:07:15 -0400

    Updated the content of the index

--- index.php
+++ index.php
@@ -1,1 +1,23 @@
 Welcome to P1
+<!DOCTYPE html>
+<html>
+<head>
+<link type="text/css" rel="stylesheet" href="index.css"/>
+<title>Saranya's Project 1.</title>
+</head>
+<body>
+
+<div class="left-background"></div>
+<div class="right-background"></div>
+
+<div class="content">
+<h1>Hi!</h1>
+<p> My name is Saranya Vijayakumar. I am 17 and am a senior at Riverdale Country School in New York. Last year I took AP Computer Science where I learned Java. I also do research at Columbia where I analyzed codon ratios and energy to find statistically significant variables that affect protein expression efficiency. I use Java, awk, and Perl for this research. I taught myself HTML and CSS. I am very interested in computer science and hope to continue studying it in college.    
+  
+  I'm excited to be learning more about web applications! </p>
+<p>I am using Adobe Dreamweaver CS6 to edit this code. </p>
+</div>
+
+
+</body>
+</html>
