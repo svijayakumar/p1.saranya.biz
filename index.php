099b1c35899266d29fdf0b065efd175c71d45171
svijayakumar <svijayakumar@riverdale.edu>
2013-09-16 23:29:37 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,16 @@
 <!DOCTYPE html>
 <html>
-<head>
+  <head>
+  
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+
 <link type="text/css" rel="stylesheet" href="index.css"/>
-<title>Saranya's Project 1.</title>
+<title>Project 1.</title>
+
+	<meta http-equiv="content-type" 
+		content="text/html;charset=utf-8" />
 </head>
 <body>
 
